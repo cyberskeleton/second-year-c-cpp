bb697e96019bca5eecd14295b727e0404e60090e
cyberskeleton <zosia.novikova@gmail.com>
2020-09-27 21:04:02 +0300

    fix conditions, replace int determinant with double

--- classwork3.c
+++ classwork3.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int function(int a, int b, int c){
-    int determinant, ans;
+    double determinant, ans;
     determinant = b*b - 4*a*c;
+    printf("Determinant: %lf, ", determinant);
     if (determinant > 0){
         ans = 2;
     }
-    if (determinant == 0){
+    else if (fabs(determinant)<0.0001) {
         ans = 1;
     }
     else{
@@ -17,9 +19,12 @@ int function(int a, int b, int c){
 
 int main(){
     int a1, a2, a3, answer;
+    printf("Enter a: ");
     scanf("%d", &a1);
+    printf("Enter b: ");
     scanf("%d", &a2);
+    printf("Enter c: ");
     scanf("%d", &a3);
     answer = function(a1, a2, a3);
-    printf("this polynomial has %d solutions", answer);
+    printf("this polynomial has %d solutions\n", answer);
 }
